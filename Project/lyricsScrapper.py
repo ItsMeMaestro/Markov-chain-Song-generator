c509eafd614fb3f4f8050669a1c8a169c6a2977a
Matvei <matvei@Matveis-MacBook-Pro.local>
2023-09-08 22:52:06 +0200

    added main() function, token is now a parameter

--- Project/lyricsScrapper.py
+++ Project/lyricsScrapper.py
@@ -1,13 +1,14 @@
 import os
 from lyricsgenius import Genius
+import argparse
 
-# Set up your Genius API client
-genius = Genius("Enter token here", timeout=10)
 """
 Scrapes the lyrics of the artist's songs and saves them to a file.
 Artist name will be used to name the output file.(case sensitive)
 Song download priority is based on popularity."""
-def scrap_lyrics(input_artist, max_songs):
+def scrap_lyrics(input_artist, max_songs, token, output_folder="lyrics"):
+    # Set up your Genius API client
+    genius = Genius(token, timeout=10)
     # Search for the artist
     artist = genius.search_artist(input_artist, max_songs=max_songs, sort="popularity")
 
@@ -18,12 +19,12 @@ def scrap_lyrics(input_artist, max_songs):
         songs[song_title] = song_lyrics
 
     # Define the folder path to save the lyrics files
-    folder_name = "lyrics"
-    os.makedirs(folder_name, exist_ok=True)  # Create the "lyrics" folder if it doesn't exist
+    
+    os.makedirs(output_folder, exist_ok=True)  # Create the "lyrics" folder if it doesn't exist
 
     # Replace spaces with underscores and create the output file name
     input_artist = artist.name.replace(" ", "")
-    output_file = os.path.join(folder_name, f"{input_artist}_{max_songs}.txt")
+    output_file = os.path.join(output_folder, f"{input_artist}_{max_songs}.txt")
     
     with open(output_file, "w", encoding="utf-8") as file:
         for title, lyrics in songs.items():
@@ -33,10 +34,13 @@ def scrap_lyrics(input_artist, max_songs):
 
 # Example usage:
 def main():
-    artist_name = "Taylor Swift"
-    max_songs = 10
-    scrap_lyrics(artist_name, max_songs)
-
+    parser = argparse.ArgumentParser(description='Scrap lyrics from Genius.com.')
+    parser.add_argument('artist', help='Name of the artist')
+    parser.add_argument('num', type=int, help='Lyrics number')
+    parser.add_argument('token', help='Genius API token')
+    parser.add_argument('--output-folder', default="lyrics", help='Output folder path')
+    args = parser.parse_args()
+    scrap_lyrics(args.artist, args.num, args.token)
 if __name__ == "__main__":
     main()
 
